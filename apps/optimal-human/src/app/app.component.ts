7fc838b27abd0bbca8a3db4f59240063c2b432f3
deepak-pc <deepak52567@gmail.com>
2021-08-09 21:37:19 +0530

    feat(OH): Add storage service provider with observable

--- apps/optimal-human/src/app/app.component.ts
+++ apps/optimal-human/src/app/app.component.ts
@@ -1,10 +1,27 @@
 import { Component } from '@angular/core';
+import { StorageMap } from '@ngx-pwa/local-storage';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'hidden-innovation-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss'],
+  styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
   title = 'optimal-human';
+
+  user$: Observable<{userData: string}> = this.storage.watch('user') as Observable<{ userData: string }>;
+
+  constructor(private storage: StorageMap) {
+
+    this.storage.set('user', {
+      'userData': 'Old User Data'
+    }).subscribe((res) => console.log(res));
+  }
+
+  updateUserData() {
+    this.storage.set('user', {
+      'userData': 'New User Data'
+    }).subscribe((res) => console.log(res));
+  }
 }
--- apps/optimal-human/src/app/app.module.ts
+++ apps/optimal-human/src/app/app.module.ts
@@ -2,12 +2,11 @@ import {NgModule} from '@angular/core';
 import {BrowserModule} from '@angular/platform-browser';
 
 import {AppComponent} from './app.component';
-import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
-import {MatButtonToggleModule} from "@angular/material/button-toggle";
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, BrowserAnimationsModule, MatButtonToggleModule],
+  imports: [BrowserModule, BrowserAnimationsModule],
   providers: [],
   bootstrap: [AppComponent],
 })
