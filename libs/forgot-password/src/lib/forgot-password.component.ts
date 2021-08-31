092c1dbd9d09af936f59547555a5d8c7c24e3ff5
Hiten Soni <hiten@nickelfox.co>
2021-08-31 10:29:24 +0530

    feat(forgot-screen): forgot password screen and validate

--- libs/forgot-password/src/lib/forgot-password.component.ts
+++ libs/forgot-password/src/lib/forgot-password.component.ts
@@ -1,4 +1,10 @@
 import { Component, OnInit, ViewEncapsulation, ChangeDetectionStrategy } from '@angular/core';
+// import {AuthFacade} from "../state/auth.facade";
+import {Validators} from "@angular/forms";
+import {FormValidationService} from "@hidden-innovation/shared/form-config";
+import {RxwebValidators} from "@rxweb/reactive-form-validators";
+import {FormControl, FormGroup} from "@ngneat/reactive-forms";
+import { AuthFacade } from '@hidden-innovation/auth';
 
 @Component({
   selector: 'hidden-innovation-forgot-password',
@@ -9,9 +15,22 @@ import { Component, OnInit, ViewEncapsulation, ChangeDetectionStrategy } from '@
 })
 export class ForgotPasswordComponent implements OnInit {
 
-  constructor() { }
+  forgotForm: FormGroup = new FormGroup({
+    email: new FormControl<string>('', [
+      RxwebValidators.email(),
+      RxwebValidators.required(),
+      Validators.required
+    ]),
+  });
+
+
+  constructor(
+    public authFacade: AuthFacade,
+    public formValidationService: FormValidationService
+  ) { }
 
   ngOnInit(): void {
+    console.log("hello password")
   }
 
 }
