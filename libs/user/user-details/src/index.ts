7ef01f63d5f1e678d70c303a6fafbc163f1455ec
Hiten Soni <hiten@nickelfox.co>
2021-09-19 17:07:58 +0530

    feat(edit):user edit ini

---
+++ libs/user/user-details/src/index.ts
@@ -0,0 +1,6 @@
+export * from './lib/state/user-details.facade';
+export * from './lib/models/user-details.interface';
+export * from './lib/state/user-details.selectors';
+export * from './lib/state/user-details.reducer';
+export * from './lib/state/user-details.actions';
+export * from './lib/user-details.module';
---
+++ libs/user/user-details/src/lib/services/user-details.service.ts
@@ -0,0 +1,25 @@
+import { Inject, Injectable } from '@angular/core';
+import { HttpClient, HttpErrorResponse, HttpParams } from '@angular/common/http';
+import { Environment, ENVIRONMENT } from '@hidden-innovation/environment';
+import { UserDetailsResponse } from '../models/user-details.interface';
+import { catchError, map } from 'rxjs/operators';
+import { Observable, throwError } from 'rxjs';
+import { UserDetails } from '@hidden-innovation/shared/models';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class UserDetailsService {
+
+  constructor(private http: HttpClient, @Inject(ENVIRONMENT) private env: Environment) {
+  }
+
+  getUserDetails(userID: number): Observable<UserDetails> {
+    let params = new HttpParams();
+    params = params.append('id', userID);
+    return this.http.get<UserDetailsResponse>(`${this.env.baseURL}/v1/admin/getSingleUser`, { params }).pipe(
+      map(res => res.data),
+      catchError((err: HttpErrorResponse) => throwError(err))
+    );
+  }
+}
