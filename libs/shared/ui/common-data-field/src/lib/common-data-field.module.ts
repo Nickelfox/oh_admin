d043f49df5200e806532ad31f618f2438e0b770c
deepak-nickelfox <deepak@nickelfox.com>
2021-09-29 03:35:24 +0530

    fix(skeleton-data-field): Fix relative import in data field component

--- libs/shared/ui/common-data-field/src/lib/common-data-field.module.ts
+++ libs/shared/ui/common-data-field/src/lib/common-data-field.module.ts
@@ -1,7 +1,7 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { CommonDataFieldComponent } from './common-data-field.component';
-import { CommonDataFieldSkeletonModule } from '../../../common-data-field-skeleton/src';
+import { CommonDataFieldSkeletonModule } from '@hidden-innovation/shared/ui/common-data-field-skeleton';
 import { ContentLoaderModule } from '@ngneat/content-loader';
 
 @NgModule({
