a7ee1c650685aa660b22cea2faba8e6fb388e3df
Hiten Soni <hiten@nickelfox.co>
2021-10-20 18:13:24 +0530

    feat(questionnaire-core): initiate

--- libs/questionnaire/create-questionnaire/src/lib/create-questionnaire.module.ts
+++ libs/questionnaire/create-questionnaire/src/lib/create-questionnaire.module.ts
@@ -1,7 +1,11 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { QuestionnaireCoreComponent } from './questionnaire-core.component';
 
 @NgModule({
   imports: [CommonModule],
+  declarations: [
+    QuestionnaireCoreComponent
+  ],
 })
 export class CreateQuestionnaireModule {}
