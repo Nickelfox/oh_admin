e40616131059e765bbb50ffeccbcb22be5762e20
Hiten Soni <hiten@nickelfox.co>
2021-10-20 18:14:30 +0530

    Revert "feat(questionnaire-core): initiate"
    This reverts commit a7ee1c650685aa660b22cea2faba8e6fb388e3df.

--- libs/questionnaire/create-questionnaire/src/lib/create-questionnaire.module.ts
+++ libs/questionnaire/create-questionnaire/src/lib/create-questionnaire.module.ts
@@ -1,11 +1,7 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { QuestionnaireCoreComponent } from './questionnaire-core.component';
 
 @NgModule({
   imports: [CommonModule],
-  declarations: [
-    QuestionnaireCoreComponent
-  ],
 })
 export class CreateQuestionnaireModule {}
