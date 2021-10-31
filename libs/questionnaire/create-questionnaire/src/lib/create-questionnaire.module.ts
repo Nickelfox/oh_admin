0d594726ee8ce42e0e6fdec25bb0450db01801e6
Hiten Soni <hiten@nickelfox.co>
2021-10-31 12:45:53 +0530

    fix(questionnaire): ui fix

--- libs/questionnaire/create-questionnaire/src/lib/create-questionnaire.module.ts
+++ libs/questionnaire/create-questionnaire/src/lib/create-questionnaire.module.ts
@@ -4,11 +4,13 @@ import { CreateQuestionnaireComponent } from './create-questionnaire.component';
 import { RouterModule } from '@angular/router';
 import { MaterialModule } from '@hidden-innovation/material';
 import { A11yModule } from '@angular/cdk/a11y';
+import { CommonFormFieldModule } from '@hidden-innovation/shared/ui/common-form-field';
 
 @NgModule({
   imports: [
     CommonModule,
     MaterialModule,
+    CommonFormFieldModule,
     RouterModule.forChild([
       {
         path: '',
