a3f23d0fdc332b7ac11c8d84345da547837bea7b
Hiten Soni <hiten@nickelfox.co>
2022-01-04 11:31:03 +0530

    feat(assessment-create): test and test group questionnaire dialog select

--- libs/assessment/assessment-create/src/lib/assessment-create.component.ts
+++ libs/assessment/assessment-create/src/lib/assessment-create.component.ts
@@ -1,4 +1,10 @@
 import { Component, OnInit, ViewEncapsulation, ChangeDetectionStrategy } from '@angular/core';
+import {ContentSelectorComponent} from "@hidden-innovation/shared/ui/content-selector";
+import {MatDialog} from "@angular/material/dialog";
+import {PackStore} from "@hidden-innovation/pack/data-access";
+import {UiStore} from "@hidden-innovation/shared/store";
+import {HotToastService} from "@ngneat/hot-toast";
+import {ActivatedRoute} from "@angular/router";
 
 @Component({
   selector: 'hidden-innovation-assessment-create',
@@ -9,9 +15,23 @@ import { Component, OnInit, ViewEncapsulation, ChangeDetectionStrategy } from '@
 })
 export class AssessmentCreateComponent implements OnInit {
 
-  constructor() { }
+  constructor(
+    private matDialog: MatDialog,
+    public store: PackStore,
+    public uiStore: UiStore,
+  ) { }
 
   ngOnInit(): void {
   }
-
+  openContentSelectorDialog(): void {
+    this.matDialog.open(ContentSelectorComponent, {
+      height: '100%',
+      width: '100%',
+      maxHeight: '100%',
+      maxWidth: '100%',
+      role: 'dialog'
+    });
+    // testGroupDialog.afterClosed().subscribe((tgs: TestGroup[] | undefined) => {
+    // });
+  }
 }
--- libs/assessment/assessment-create/src/lib/assessment-create.module.ts
+++ libs/assessment/assessment-create/src/lib/assessment-create.module.ts
@@ -1,25 +1,35 @@
 import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
+import {CommonModule, TitleCasePipe, UpperCasePipe} from '@angular/common';
 import { AssessmentCreateComponent } from './assessment-create.component';
 import {MaterialModule} from "@hidden-innovation/material";
 import {CommonFormFieldModule} from "@hidden-innovation/shared/ui/common-form-field";
 import {CommonFormFieldImageModule} from "@hidden-innovation/shared/ui/common-form-field-image";
 import {UtilsModule} from "@hidden-innovation/shared/utils";
 import {CommonFormFieldTextareaModule} from "@hidden-innovation/shared/ui/common-form-field-textarea";
-import {CommonFormFieldVideoModule} from "@hidden-innovation/shared/ui/common-form-field-video";
 import {FormFieldErrorsModule} from "@hidden-innovation/shared/ui/form-field-errors";
 import {RouterModule} from "@angular/router";
+import {PackDataAccessModule} from "@hidden-innovation/pack/data-access";
+import {CommonFormFieldFileModule} from "@hidden-innovation/shared/ui/common-form-field-file";
+import {MediaModule} from "@hidden-innovation/media";
+import {TagsDataAccessModule} from "@hidden-innovation/tags/data-access";
+import {ContentSelectorModule} from "@hidden-innovation/shared/ui/content-selector";
+import {PackContentCardModule} from "@hidden-innovation/shared/ui/pack-content-card";
 
 @NgModule({
   imports: [
     CommonModule,
     MaterialModule,
     CommonFormFieldModule,
-    CommonFormFieldImageModule,
-    UtilsModule,
     CommonFormFieldTextareaModule,
-    CommonFormFieldVideoModule,
+    CommonFormFieldImageModule,
+    CommonFormFieldFileModule,
+    MediaModule,
+    TagsDataAccessModule,
+    ContentSelectorModule,
+    UtilsModule,
+    PackDataAccessModule,
     FormFieldErrorsModule,
+    PackContentCardModule,
     RouterModule.forChild([
       {
         path: '',
@@ -31,5 +41,9 @@ import {RouterModule} from "@angular/router";
   declarations: [
     AssessmentCreateComponent
   ],
+  providers: [
+    UpperCasePipe,
+    TitleCasePipe
+  ]
 })
 export class AssessmentCreateModule {}
