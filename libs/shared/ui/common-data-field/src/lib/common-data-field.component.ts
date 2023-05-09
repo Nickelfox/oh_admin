2b70eb544048aac16253c903cf75dc98d4ceb96a
hiten-nickelfox <hiten-nickelfox@nickelfox.co>
2023-05-09 09:15:49 +0530

    fix(user): Goals repeat fix

--- libs/shared/ui/common-data-field/src/lib/common-data-field.component.ts
+++ libs/shared/ui/common-data-field/src/lib/common-data-field.component.ts
@@ -1,5 +1,5 @@
 import {ChangeDetectionStrategy, Component, Input, ViewEncapsulation} from '@angular/core';
-import {GoalAnswer} from '@hidden-innovation/shared/models';
+import {GoalAnswer, NewGoal} from '@hidden-innovation/shared/models';
 
 @Component({
   selector: 'hidden-innovation-common-data-field',
@@ -12,7 +12,7 @@ import {GoalAnswer} from '@hidden-innovation/shared/models';
       ></hidden-innovation-shimmer>
       <h4 *ngIf='!goals' class='mat-body-2 font-weight-light'>{{value || '--'}}</h4>
       <ol type="number"  class="p-0 pl-3">
-        <li *ngFor="let goal of goals"  class='mat-body-2 font-weight-light goalText p-0'><h4 class='mat-body-2 font-weight-light'>{{goal.answerString || "--"}}</h4></li>
+        <li *ngFor="let goal of goals"  class='mat-body-2 font-weight-light goalText p-0'><h4 class='mat-body-2 font-weight-light'>{{goal.goalAnswer.answerString || "--"}}</h4></li>
       </ol>
     </hidden-innovation-common-data-field-skeleton>
   `,
@@ -22,7 +22,7 @@ import {GoalAnswer} from '@hidden-innovation/shared/models';
 })
 export class CommonDataFieldComponent {
   @Input() label?: string | null;
-  @Input() goals?: GoalAnswer[];
+  @Input() goals?: NewGoal[] | null;
   @Input() value?: string | number | null | undefined;
   @Input() isLoading?: boolean | null;
 
